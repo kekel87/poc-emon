af849401915b1418312db3762d2cca0aee0e1df8
Michael Parry <michael.parry.87@gmail.com>
2022-11-05 19:25:30 +0100

    feat(symfony): add team ApiResource

--- backs/symfony/migrations/Version20220730085449.php
+++ backs/symfony/migrations/Version20220730085449.php
@@ -26,12 +26,20 @@ final class Version20220730085449 extends AbstractMigration implements Container
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE pokemon (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE team (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, pokemon1_id INTEGER DEFAULT NULL, pokemon2_id INTEGER DEFAULT NULL, pokemon3_id INTEGER DEFAULT NULL, pokemon4_id INTEGER DEFAULT NULL, pokemon5_id INTEGER DEFAULT NULL, pokemon6_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, owner VARCHAR(255) NOT NULL, CONSTRAINT FK_C4E0A61F1722D724 FOREIGN KEY (pokemon1_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C4E0A61F59778CA FOREIGN KEY (pokemon2_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C4E0A61FBD2B1FAF FOREIGN KEY (pokemon3_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C4E0A61F20FC2716 FOREIGN KEY (pokemon4_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C4E0A61F98404073 FOREIGN KEY (pokemon5_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C4E0A61F8AF5EF9D FOREIGN KEY (pokemon6_id) REFERENCES pokemon (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61F1722D724 ON team (pokemon1_id)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61F59778CA ON team (pokemon2_id)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61FBD2B1FAF ON team (pokemon3_id)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61F20FC2716 ON team (pokemon4_id)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61F98404073 ON team (pokemon5_id)');
+        $this->addSql('CREATE INDEX IDX_C4E0A61F8AF5EF9D ON team (pokemon6_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE pokemon');
+        $this->addSql('DROP TABLE team');
     }
 
     /**
---
+++ backs/symfony/src/Entity/Team.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ApiResource]
+class Team
+{
+  #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+  private ?int $id = null;
+
+  #[ORM\Column]
+  #[Assert\NotBlank]
+  public string $name;
+
+  #[ORM\Column]
+  #[Assert\NotBlank]
+  public string $owner;
+
+  #[ORM\ManyToOne]
+  #[Assert\NotBlank]
+  public Pokemon $pokemon1;
+
+  #[ORM\ManyToOne]
+  public Pokemon $pokemon2;
+
+  #[ORM\ManyToOne]
+  public Pokemon $pokemon3;
+
+  #[ORM\ManyToOne]
+  public Pokemon $pokemon4;
+
+  #[ORM\ManyToOne]
+  public Pokemon $pokemon5;
+
+  #[ORM\ManyToOne]
+  public Pokemon $pokemon6;
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+}
